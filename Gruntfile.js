f6c6ff87f85ff141930471e85a12bff8c7fd861e
Farook Ibrahim <farook@transvelo.com>
2019-03-27 18:39:52 +0530

    grunt changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,6 +86,7 @@ module.exports = function( grunt ) {
                         '!Gruntfile.js',
                         '!README.md',
                         '!package.json',
+                        '!package-lock.json',
                         '!node_modules/**',
                         '!none',
                         '!.DS_Store',
