bb02ae1b85f81a5e919967ebe7f543f51fa29f40
Farook Ibrahim <farook@transvelo.com>
2019-03-27 18:28:20 +0530

    grunt added

---
+++ Gruntfile.js
@@ -0,0 +1,114 @@
+/* jshint node:true */
+module.exports = function( grunt ) {
+    'use strict';
+
+    grunt.initConfig({
+
+        //Metadata
+        pkg: grunt.file.readJSON( 'package.json' ),
+
+        // Generate POT files.
+        makepot: {
+            options: {
+                type: 'wp-plugin',
+                domainPath: 'languages',
+                potHeaders: {
+                    'report-msgid-bugs-to': 'https://github.com/madrasthemes/mas-static-blocks/issues',
+                    'language-team': 'LANGUAGE <EMAIL@ADDRESS>'
+                }
+            },
+            dist: {
+                options: {
+                    potFilename: 'mas-static-blocks.pot',
+                    exclude: [
+                        'apigen/.*',
+                        'tests/.*',
+                        'tmp/.*'
+                    ]
+                }
+            }
+        },
+
+        // Check textdomain errors.
+        checktextdomain: {
+            options:{
+                text_domain: 'mas-static-blocks',
+                keywords: [
+                    '__:1,2d',
+                    '_e:1,2d',
+                    '_x:1,2c,3d',
+                    'esc_html__:1,2d',
+                    'esc_html_e:1,2d',
+                    'esc_html_x:1,2c,3d',
+                    'esc_attr__:1,2d',
+                    'esc_attr_e:1,2d',
+                    'esc_attr_x:1,2c,3d',
+                    '_ex:1,2c,3d',
+                    '_n:1,2,4d',
+                    '_nx:1,2,4c,5d',
+                    '_n_noop:1,2,3d',
+                    '_nx_noop:1,2,3c,4d'
+                ]
+            },
+            files: {
+                src:  [
+                    '**/*.php',         // Include all files
+                    '!apigen/**',       // Exclude apigen/
+                    '!node_modules/**', // Exclude node_modules/
+                    '!tests/**',        // Exclude tests/
+                    '!vendor/**',       // Exclude vendor/
+                    '!tmp/**'           // Exclude tmp/
+                ],
+                expand: true
+            }
+        },
+
+        // Clean the directory.
+        clean: {
+            dist: [
+                '<%= pkg.name %>*.zip'
+            ]
+        },
+
+        compress: {
+            build: {
+                options: {
+                    archive: '<%= pkg.name %>.zip',
+                    mode: 'zip'
+                },
+                files: [ {
+                    expand: true,
+                    src: [
+                        '**',
+                        '!.*',
+                        '!.*/**',
+                        '.htaccess',
+                        '!Gruntfile.js',
+                        '!README.md',
+                        '!package.json',
+                        '!node_modules/**',
+                        '!none',
+                        '!.DS_Store',
+                        '!npm-debug.log'
+                    ]
+                } ]
+            }
+        }
+    });
+
+    // Load NPM tasks to be used here
+    grunt.loadNpmTasks( 'grunt-wp-i18n' );
+    grunt.loadNpmTasks( 'grunt-checktextdomain' );
+    grunt.loadNpmTasks( 'grunt-contrib-clean' );
+    grunt.loadNpmTasks( 'grunt-contrib-compress' );
+
+    // Register tasks
+    grunt.registerTask( 'default', [
+        'checktextdomain',
+        'makepot'
+    ]);
+
+    grunt.registerTask( 'deploy', [
+        'compress:build'
+    ]);
+};
