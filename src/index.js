cbb9247da45b414e3c397635b611d539e4e388ae
niloferfarzana <nilofer@transvelo.com>
2025-09-22 19:37:16 +0530

    added static-content block

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const { registerBlockType } = wp.blocks;
+const { SelectControl } = wp.components;
+const { useSelect } = wp.data;
+
+registerBlockType('mytheme/navigation-static-content', {
+    title: 'Static Content Menu',
+    icon: 'screenoptions',
+    category: 'widgets',
+    attributes: {
+        staticContentId: { type: 'number' }
+    },
+    edit: (props) => {
+        const { attributes, setAttributes } = props;
+        const posts = useSelect(select => 
+            select('core').getEntityRecords('postType', 'mas_static_content', { per_page: -1 }), 
+            []
+        ) || [];
+
+        return (
+            <SelectControl
+                label="Select Static Content"
+                value={attributes.staticContentId}
+                options={posts.map(post => ({ label: post.title.rendered, value: post.id }))}
+                onChange={(value) => setAttributes({ staticContentId: parseInt(value) })}
+            />
+        );
+    },
+    save: () => null // Always rendered server-side
+});
